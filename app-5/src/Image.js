49060fe98dc070ce8308cf8d631a8446288737e7
Joely Vernier <55214971+jorovernier@users.noreply.github.com>
2019-10-01 15:06:41 -0700

    Answered question 5

---
+++ app-5/src/Image.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+
+export default class Image extends Component{
+    render(){
+       return <img src='https://a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2019/09/931/524/cat-getty-images.jpg?ve=1&tl=1' />
+    }
+}
